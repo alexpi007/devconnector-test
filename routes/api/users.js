e3c39217840b0fbe4668a63bdebd1bc2bade0eaf
Alessandro Pilastrini <alessandropilastrini@gmail.com>
2018-09-27 21:38:41 +0200

    json web token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,28 +1,30 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load User model
-const User = require("../../models/User");
+const User = require('../../models/User');
 
 // @route GET api/users/test
 // @desc Test users route
 // @access Public
-router.get("/test", (req, res) => res.json({ msg: "Users works" }));
+router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
 
 // @route GET api/users/register
 // @desc Register user
 // @access Public
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exist" });
+      return res.status(400).json({ email: 'Email already exist' });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: 200, // Size
-        r: "pg", // Rating
-        d: "mm" // Default
+        r: 'pg', // Rating
+        d: 'mm' // Default
       });
 
       const newUser = new User({
@@ -50,7 +52,7 @@ router.post("/register", (req, res) => {
 // @desc Login User / Returning JWT Token
 // @access Public
 
-router.post("/login", (req, res) => {
+router.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
@@ -58,15 +60,34 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      return res.status(404).json({ email: 'User not found' });
     }
 
     // Check if password typed (plain text) match with one stored on DB (encrypted hash)
     bcrypt.compare(password, user.password).then(pswIsMatch => {
       if (pswIsMatch) {
-        res.json({ msg: "Succes" });
+        // User matched
+
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        }; // Creates JWT payload
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          }
+        ); // 1 hour
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        return res.status(400).json({ password: 'Password incorrect' });
       }
     });
   });
